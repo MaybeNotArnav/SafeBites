7aa219570359938e50801a6e2b96d0b6a49950cc
miaglenn <miaxiaoqiao11@gmail.com>
2025-10-19 16:58:02 -0400

    Create dedicated files for all dashboard sub-pages

---
+++ proj2/SafeBites/frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h2>Home</h2>
+      <p className="wip-message">Work in progress... This page is under construction.</p>
+    </div>
+  );
+}
+
+export default Home;
