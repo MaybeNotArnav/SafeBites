bc07c3eb4af4df070756ad3130dd7442ca104d46
Richa Jha <richajha749@gmail.com>
2025-11-01 00:20:19 -0400

    Update

--- proj2/SafeBites/backend/app/services/restaurant_service.py
+++ proj2/SafeBites/backend/app/services/restaurant_service.py
@@ -1,9 +1,8 @@
-from bson import ObjectId
-from fastapi import HTTPException
-from app.models.restaurant_model import RestaurantCreate, RestaurantUpdate, RestaurantInDB
-from app.db import get_db
+# from bson import ObjectId
+# from fastapi import HTTPException
+# from app.models.restaurant_model import RestaurantCreate, RestaurantUpdate, RestaurantInDB
+# from app.db import get_db
 
-db = get_db()
+# db = get_db()
 
-def create_restaurant(restaurant:RestaurantCreate):
-    
+# def create_restaurant(restaurant:RestaurantCreate):
